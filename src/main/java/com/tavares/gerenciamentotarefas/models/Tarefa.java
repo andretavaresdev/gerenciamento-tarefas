4136f474e2536d37aef1601c29b25919c88a8423
Andre Tavares <andreetavaress1@gmail.com>
2026-03-24 17:26:33 -0300

    add: classe Tarefa criada

---
+++ src/main/java/com/tavares/gerenciamentotarefas/models/Tarefa.java
@@ -0,0 +1,55 @@
+package com.tavares.gerenciamentotarefas.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="tb_tarefa")
+public class Tarefa {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="descricao_tarefa")
+    private String descricao;
+
+    @Column(name="data_validade")
+    private LocalDate dataVencimento;
+
+    private Boolean concluida;
+
+    public Tarefa(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
